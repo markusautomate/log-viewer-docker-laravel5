c62644c07d674dff0254266d3b78627293991fe8
markujavier <markus@agencyautomate.com>
2023-02-25 06:02:59 +0800

    wip: fixed mcrypt deprecation

--- src/config/app.php
+++ src/config/app.php
@@ -80,7 +80,7 @@ return [
 
 	'key' => env('APP_KEY', 'SomeRandomString'),
 
-	'cipher' => MCRYPT_RIJNDAEL_128,
+	'cipher' => null,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -121,7 +121,8 @@ return [
 		'Illuminate\Routing\ControllerServiceProvider',
 		'Illuminate\Cookie\CookieServiceProvider',
 		'Illuminate\Database\DatabaseServiceProvider',
-		'Illuminate\Encryption\EncryptionServiceProvider',
+//		'Illuminate\Encryption\EncryptionServiceProvider',
+		'Thomaswelton\LaravelMcryptFaker\NoEncryptionServiceProvider',
 		'Illuminate\Filesystem\FilesystemServiceProvider',
 		'Illuminate\Foundation\Providers\FoundationServiceProvider',
 		'Illuminate\Hashing\HashServiceProvider',
